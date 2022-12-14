52732eacabbd52afa9ffa868bc33ac69b9516392
leonardobarreras <leonardogbarreras99@gmail.com>
2022-12-14 00:43:38 -0500

    Añadir parametros globales
    Se añaden las bibliotecas a utlizar y se definen los parametros globales del juego

---
+++ snake_gamepy.py
@@ -0,0 +1,26 @@
+import pygame
+import time
+import random
+ 
+pygame.init()
+ 
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+ 
+dis_width = 600
+dis_height = 400
+ 
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Juego de la serpiente')
+ 
+clock = pygame.time.Clock()
+ 
+snake_block = 10
+snake_speed = 15
+ 
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
