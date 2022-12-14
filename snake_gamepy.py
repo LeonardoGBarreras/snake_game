606aa7c21f900035d4791f2332569c8bb9d009a5
leonardobarreras <leonardogbarreras99@gmail.com>
2022-12-14 00:47:29 -0500

    Añadir funciones
    Se añaden las funciones para calcular el score, para mostrar mensajes y para dibujar a la serpiente

--- snake_gamepy.py
+++ snake_gamepy.py
@@ -24,3 +24,24 @@ snake_speed = 15
  
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
+
+
+def Your_score(score):
+    value = score_font.render("Puntuación: " + str(score), True, black)
+    dis.blit(value, [0, 0])
+ 
+ 
+ 
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+ 
+ 
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+ 
+ 
+def message2(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 2])
